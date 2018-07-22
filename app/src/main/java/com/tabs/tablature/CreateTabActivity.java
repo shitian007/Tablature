dae95b2b658f3b0fb3e64318bc329bb89df4d12e
Shi Tian <shitian007@hotmail.com>
2018-07-22 11:14:43 +0800

    Change create tab to new activity

---
+++ app/src/main/java/com/tabs/tablature/CreateTabActivity.java
@@ -0,0 +1,25 @@
+package com.tabs.tablature;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class CreateTabActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_tab);
+
+        hideSystemUI();
+    }
+
+    // Enable stick immersive display on full screen
+    private void hideSystemUI() {
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
+    }
+}
--- app/src/main/java/com/tabs/tablature/MainActivity.java
+++ app/src/main/java/com/tabs/tablature/MainActivity.java
@@ -1,5 +1,6 @@
 package com.tabs.tablature;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -12,7 +13,6 @@ import android.view.View;
 public class MainActivity extends AppCompatActivity {
 
     private HomeFragment homeFragment = new HomeFragment();
-    private CreateTabFragment createTabFragment = new CreateTabFragment();
     private ProfileFragment profileFragment = new ProfileFragment();
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
@@ -20,15 +20,14 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            showSystemUI();
             switch (item.getItemId()) {
                 case R.id.navigation_home:
                     setTitle("Tablature");
                     setFragment(homeFragment);
                     return true;
                 case R.id.navigation_dashboard:
-                    hideSystemUI();
-                    setFragment(createTabFragment);
+                    Intent createTabIntent = new Intent(MainActivity.this, CreateTabActivity.class);
+                    startActivity(createTabIntent);
                     return true;
                 case R.id.navigation_notifications:
                     setTitle("User Profile");
@@ -47,16 +46,8 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
+        showSystemUI();
         setFragment(homeFragment);
-    }
-
-    // Enable stick immersive display on full screen
-    private void hideSystemUI() {
-        View decorView = getWindow().getDecorView();
-        decorView.setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
     }
 
     private void showSystemUI() {
