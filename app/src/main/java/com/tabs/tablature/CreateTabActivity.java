f99ca088915e951b8b9943fcc8e43fabba36e5a2
Shi Tian <shitian007@hotmail.com>
2018-07-22 12:40:26 +0800

    Set sensor landscape for create tab activity

--- app/src/main/java/com/tabs/tablature/CreateTabActivity.java
+++ app/src/main/java/com/tabs/tablature/CreateTabActivity.java
@@ -1,5 +1,6 @@
 package com.tabs.tablature;
 
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,6 +14,7 @@ public class CreateTabActivity extends AppCompatActivity {
 
         // TODO: Change orientation to landscape
         hideSystemUI();
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR_LANDSCAPE);
     }
 
     // Enable stick immersive display on full screen
