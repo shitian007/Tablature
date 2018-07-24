67e364a76cf8b4734112af8279494244cc4d4fa1
Shi Tian <shitian007@hotmail.com>
2018-07-24 13:46:16 +0800

    Implement basic drag and drop on Crochet

--- app/src/main/java/com/tabs/tablature/CreateTabActivity.java
+++ app/src/main/java/com/tabs/tablature/CreateTabActivity.java
@@ -3,18 +3,58 @@ package com.tabs.tablature;
 import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 public class CreateTabActivity extends AppCompatActivity {
+
+    private ImageView crochet;
+    private ViewGroup rootLayout;
+    private int _xDelta;
+    private int _yDelta;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_tab);
 
-        // TODO: Change orientation to landscape
-        hideSystemUI();
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR_LANDSCAPE);
+        hideSystemUI();
+
+        rootLayout = findViewById(R.id.create_tab_root);
+        crochet = findViewById(R.id.crochet);
+        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(150, 150);
+        crochet.setLayoutParams(layoutParams);
+        crochet.setOnTouchListener(new ChoiceTouchListener());
+    }
+
+    private final class ChoiceTouchListener implements View.OnTouchListener {
+        @Override
+        public boolean onTouch(View view, MotionEvent motionEvent) {
+            final int X = (int) motionEvent.getRawX();
+            final int Y = (int) motionEvent.getRawY();
+            RelativeLayout.LayoutParams lParams = (RelativeLayout.LayoutParams) view.getLayoutParams();
+            switch(motionEvent.getAction() & MotionEvent.ACTION_MASK) {
+                case MotionEvent.ACTION_DOWN:
+                    _xDelta = X - lParams.leftMargin;
+                    _yDelta = Y - lParams.topMargin;
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                    lParams.leftMargin = X - _xDelta;
+                    lParams.topMargin = Y - _yDelta;
+                    lParams.rightMargin = -250;
+                    lParams.bottomMargin = -250;
+                    view.setLayoutParams(lParams);
+                    break;
+                default:
+                    break;
+            }
+            rootLayout.invalidate();
+            return true;
+        }
     }
 
     // Enable stick immersive display on full screen
