1afc5fccd1c8718b4d0363c64c41360f1b1226e4
Shi Tian <shitian007@hotmail.com>
2018-07-24 23:18:40 +0800

    Add class TablatureFileIO

---
+++ app/src/main/java/com/tabs/tablature/framework/implementation/TablatureFileIO.java
@@ -0,0 +1,37 @@
+package com.tabs.tablature.framework.implementation;
+
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.os.Environment;
+
+import com.tabs.tablature.framework.interfaces.FileIO;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class TablatureFileIO implements FileIO {
+
+    Context context;
+    AssetManager assets;
+    String externalStoragePath;
+
+    public TablatureFileIO(Context context) {
+        this.context =context;
+        this.assets =context.getAssets();
+        this.externalStoragePath = Environment.getExternalStorageDirectory()
+                .getAbsolutePath() + File.separator;
+    }
+    public InputStream readAsset(String fileName) throws IOException {
+        return assets.open(fileName);
+    }
+    public InputStream readFile(String fileName) throws IOException {
+        return new FileInputStream(externalStoragePath+fileName);
+    }
+    public OutputStream writeFile(String fileName) throws IOException {
+        return new FileOutputStream(externalStoragePath+fileName);
+    }
+}
