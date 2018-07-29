0e6d6d2869bb5c6789a0e3dd1cfa180bf635b568
Shi Tian <shitian007@hotmail.com>
2018-07-29 09:44:36 +0800

    Allow wake-lock and add GLSurfaceView

--- app/src/main/java/com/tabs/tablature/CreateTabActivity.java
+++ app/src/main/java/com/tabs/tablature/CreateTabActivity.java
@@ -1,59 +1,73 @@
 package com.tabs.tablature;
 
+import android.content.Context;
 import android.content.pm.ActivityInfo;
+import android.opengl.GLSurfaceView;
+import android.os.PowerManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.MotionEvent;
+import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.RelativeLayout;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.tabs.tablature.framework.implementation.TablatureAudio;
+import com.tabs.tablature.framework.implementation.TablatureFileIO;
+
+import java.util.Random;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+import javax.microedition.khronos.opengles.GL11;
 
 public class CreateTabActivity extends AppCompatActivity {
 
-    private ImageView crochet;
-    private ViewGroup rootLayout;
-    private int _xDelta;
-    private int _yDelta;
+    GLSurfaceView glView;
+    TablatureFileIO tablatureFileIO;
+    TablatureAudio tablatureAudio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_tab);
 
+        // Wake-lock for screen timeout
+        PowerManager powerManager = (PowerManager)this.getSystemService(Context.POWER_SERVICE);
+        PowerManager.WakeLock wakeLock = powerManager.newWakeLock(PowerManager.FULL_WAKE_LOCK, "My Lock");
+        wakeLock.acquire();
+        // TODO: Extend GLSurfaceView to have OpenGL inside layout
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR_LANDSCAPE);
         hideSystemUI();
 
-        rootLayout = findViewById(R.id.create_tab_root);
-        crochet = findViewById(R.id.crochet);
-        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(150, 150);
-        crochet.setLayoutParams(layoutParams);
-        crochet.setOnTouchListener(new ChoiceTouchListener());
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        glView = new GLSurfaceView(this);
+        glView.setRenderer(new SimpleRenderer());
+        setContentView(glView);
+
+        tablatureFileIO = new TablatureFileIO(this);
+        tablatureAudio = new TablatureAudio(this);
     }
 
-    private final class ChoiceTouchListener implements View.OnTouchListener {
+    static class SimpleRenderer implements GLSurfaceView.Renderer {
+        Random rand = new Random();
+
         @Override
-        public boolean onTouch(View view, MotionEvent motionEvent) {
-            final int X = (int) motionEvent.getRawX();
-            final int Y = (int) motionEvent.getRawY();
-            RelativeLayout.LayoutParams lParams = (RelativeLayout.LayoutParams) view.getLayoutParams();
-            switch(motionEvent.getAction() & MotionEvent.ACTION_MASK) {
-                case MotionEvent.ACTION_DOWN:
-                    _xDelta = X - lParams.leftMargin;
-                    _yDelta = Y - lParams.topMargin;
-                    break;
-                case MotionEvent.ACTION_MOVE:
-                    lParams.leftMargin = X - _xDelta;
-                    lParams.topMargin = Y - _yDelta;
-                    lParams.rightMargin = -250;
-                    lParams.bottomMargin = -250;
-                    view.setLayoutParams(lParams);
-                    break;
-                default:
-                    break;
-            }
-            rootLayout.invalidate();
-            return true;
+        public void onSurfaceCreated(GL10 gl10, EGLConfig eglConfig) {
+            Log.d("GLSurfaceViewTest", "surface created");
+
+        }
+
+        @Override
+        public void onSurfaceChanged(GL10 gl10, int i, int i1) {
+            Log.d("GLSurfaceViewTest", "surface changed: "+ i +"x" + i1);
+        }
+
+        @Override
+        public void onDrawFrame(GL10 gl10) {
+            gl10.glClearColor(rand.nextFloat(), rand.nextFloat(),
+                    rand.nextFloat(), 1);
+            gl10.glClear(GL11.GL_COLOR_BUFFER_BIT);
         }
     }
 
