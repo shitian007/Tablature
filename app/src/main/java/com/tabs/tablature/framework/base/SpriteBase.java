e65277ae82340ecbfea6839fda5dfb038aa56d26
Shi Tian <shitian007@hotmail.com>
2018-08-03 13:06:10 +0800

    Add SpriteBase class

---
+++ app/src/main/java/com/tabs/tablature/framework/base/SpriteBase.java
@@ -0,0 +1,30 @@
+package com.tabs.tablature.framework.base;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+
+public class SpriteBase {
+
+    private Bitmap image;
+
+    public int x;
+    public int y;
+
+    public SpriteBase(Bitmap bitmap, int initialX, int initialY) {
+        this.image = bitmap;
+        this.x = initialX;
+        this.y = initialY;
+    }
+
+    public int getWidth() {
+        return this.image.getWidth();
+    }
+
+    public int getHeight() {
+        return this.image.getHeight();
+    }
+
+    public void draw(Canvas canvas) {
+        canvas.drawBitmap(image, x, y, null);
+    }
+}
