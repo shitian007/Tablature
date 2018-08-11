7e9839ff14be6026034d4cfcd475b6110871dc3b
Shi Tian <shitian007@hotmail.com>
2018-08-11 10:32:50 +0800

    Add Stave Class

---
+++ app/src/main/java/com/tabs/tablature/framework/base/Stave.java
@@ -0,0 +1,10 @@
+package com.tabs.tablature.framework.base;
+
+import android.graphics.Bitmap;
+
+public class Stave extends SpriteBase {
+
+    public Stave(Bitmap bitmap, float initialX, float initialY) {
+        super(bitmap, initialX, initialY);
+    }
+}
